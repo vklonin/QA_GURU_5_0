f9c1477dfd53d2250f320fbbdfb0315acb984ec4
vladimir klonin <vklonin@gmail.com>
2021-02-13 08:50:31 +0400

    @steps file added

--- src/test/java/GithubCheckingIssueNameSteps.java
+++ src/test/java/GithubCheckingIssueNameSteps.java
@@ -1,2 +1,22 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
 public class GithubCheckingIssueNameSteps {
+
+    String REPOSITORY = "vklonin/QA_GURU_2_0";
+    String ISSUE_NAME = "Just to check if it exists";
+
+    @Test
+    public void webInterfaceCheck(){
+        open("https://github.com/");
+        $(byAttribute("name", "q")).sendKeys(REPOSITORY);
+        $(byAttribute("name", "q")).pressEnter();
+        $(byAttribute("href", "/"+REPOSITORY)).click();
+        $(by("data-hotkey", "g i")).click();
+        $(withText(ISSUE_NAME)).should(Condition.exist);
+    }
 }
